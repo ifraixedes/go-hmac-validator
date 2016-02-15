f82c70633374cf95fb9119832040336efa8245a3
Ivan Fraixedes <ivan@fraixed.es>
2016-02-15 17:25:49 +0000

    Test & implemenation for: hmac signature value extraction & keys exclusion

--- validator.go
+++ validator.go
@@ -1,12 +1,14 @@
 package hmacval
 
+import "errors"
+
 /*
 
 Process to validate an HMAC digest signature:
 
 Take map with string type keys and values (I'll call it 'payload' onwards)
- 1. If HMAC digest is inside of the payload then extract it and exclude it from the HMAC signature
- 2. Exclude keys which shouldn't be considered to perform the HMAC signature
+ 1. If HMAC digest is inside of the payload then extract it and exclude it from the HMAC signature - DONE (hmacSigValue func)!
+ 2. Exclude keys which shouldn't be considered to perform the HMAC signature - DONE (mapToSlice func)!
  3. Performs character replacement on the payload keys, values or both
  4. Sort the payload key/values lexicographically
  5. Join the payload Keys and its values with a specified string or none
@@ -14,3 +16,42 @@ Take map with string type keys and values (I'll call it 'payload' onwards)
  7. Compute the digest on the payload with the specified secret and required encoding
  8. Compare the resulted digest with the provided one (in the payload or aside) and returns if it matches or not.
 */
+
+// ErrSigKeyNotFound is returned when payload doesn't contain a specified key with its own signature as a value
+var ErrSigKeyNotFound = errors.New("Signature key not found in payload")
+
+// hmacSignValue returns from payload, the value associated with the specified, if it exists, otherwise it returns ErrSigKeyNotFound
+func hmacSigValue(key string, payload map[string]string) (string, error) {
+	sig, ok := payload[key]
+
+	if !ok {
+		return "", ErrSigKeyNotFound
+	}
+
+	return sig, nil
+}
+
+// mapToSlice returns a slice of key & value pairs excluding those which are in keysToExclude slice
+func mapToSlice(keysToExclude []string, payload map[string]string) []string {
+	result := make([]string, 0, len(payload)*2)
+
+PayloadLoop:
+	for k, v := range payload {
+		if keysToExclude != nil {
+			for _, e := range keysToExclude {
+				if e == k {
+					continue PayloadLoop
+				}
+			}
+		}
+
+		result = append(result, k, v)
+	}
+
+	return result
+}
+
+// makeReplacements replace the old, new string pairs of the slice of key/value pairs.
+// k make replacement on keys (odd idexes of pairs) and v on values (even indexes of pairs).
+func makeReplacements(k []string, v []string, pairs []string) {
+}
--- validator_test.go
+++ validator_test.go
@@ -1,55 +1,110 @@
-package hmacval_test
+package hmacval
 
-import "testing"
+import (
+	"sort"
+	"testing"
 
-func TestExtractHMACSignatureFromPayload(t *testing.T) {
-	t.Error("Not implemented")
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHmacSigValue(t *testing.T) {
+	type expect struct {
+		key     string
+		payload map[string]string
+		sig     string
+		err     error
+	}
+
+	expectations := []expect{
+		expect{"hmac", map[string]string{"key1": "value1", "key2": "value2", "hmac": "the-signature"}, "the-signature", nil},
+		expect{"signature", map[string]string{"key1": "value1", "key2": "value2", "signature": "the-signature"}, "the-signature", nil},
+		expect{"hmac", map[string]string{"key1": "value1", "key2": "value2", "signature": "the-signature"}, "", ErrSigKeyNotFound},
+	}
+
+	for _, e := range expectations {
+		sig, err := hmacSigValue(e.key, e.payload)
+		assert.Equal(t, e.sig, sig, "Sinature value doen't match")
+		assert.Equal(t, e.err, err, "Returned error value doesn't match")
+	}
 }
 
-func TestExcludeKeys(t *testing.T) {
-	t.Error("Not implemented")
+func TestMapToSlice(t *testing.T) {
+	type expect struct {
+		keysToExclude []string
+		payload       map[string]string
+		result        []string
+	}
+
+	expectations := []expect{
+		expect{[]string{"key2", "key3"}, map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}, []string{"key1", "value1"}},
+		expect{[]string{"key1"}, map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}, []string{"key2", "value2", "key3", "value3"}},
+		expect{[]string{"key2", "not-exist"}, map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}, []string{"key1", "value1", "key3", "value3"}},
+		expect{[]string{"key1", "key2", "key3"}, map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}, []string{}},
+		expect{[]string{}, map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}, []string{"key1", "value1", "key2", "value2", "key3", "value3"}},
+		expect{nil, map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}, []string{"key1", "value1", "key2", "value2", "key3", "value3"}},
+	}
+
+	for _, e := range expectations {
+		result := mapToSlice(e.keysToExclude, e.payload)
+		eres := sort.StringSlice(e.result)
+		res := sort.StringSlice(result)
+
+		eres.Sort()
+		res.Sort()
+		assert.EqualValues(t, eres, res, "Result doesn't match the expectation")
+	}
 }
 
 func TestReplaceCharacters(t *testing.T) {
 	// Replace characters is possible to achieve with strings.Replacer
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestSortKeyValuePairs(t *testing.T) {
 }
 
 func TestJoinKeyValue(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestJoinKeysValuesPairs(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestCalcuateDigest(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestAuthSignature_WithNoReplacements(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestAuthSignature_WithHMACInside(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestAuthSignature_WithHMACAside(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestShopifyAuthSignature(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestTwilioAuthSignature(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
 
 func TestPusherthSignature(t *testing.T) {
-	t.Error("Not implemented")
+	t.Log("Not implemented")
+	t.Skip()
 }
